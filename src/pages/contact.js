813bfe72f1be1c18dc99457d498000d530ddb518
michals-src <54778121+michals-src@users.noreply.github.com>
2021-10-10 18:42:09 +0200

    Zmiany kontakt 10.10.2021

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -169,7 +169,123 @@ export default function Contact() {
             <title>Selleads - Kontakt</title>
         </Helmet>
 
+        <div className="py-14 bg-gradient-to-b from-gray-200 to-gray-white ">
             <Navbar />
+
+            <div className="container pr-4 md:pr-14 mx-auto">
+                <div className="flex flex-row flex-nowrap">
+                    <div className="w-2/12"></div>
+                    <div className="w-10/12">
+                        <div className="max-w-3xl">
+                            <div className="my-16">
+                                <h1 className="text-6xl leading-tight font-bold">
+                                    Bądźmy w kontakcie.
+                                </h1>
+                                <p className="mt-5 text-lg">
+                                    Jeśli chcesz dowiedzieć się czegoś więcej, po prostu do nas napisz
+                                    <span className="mx-1 text-green-800 inline-flex">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                    </span>
+                                </p>
+                                <p className="mb-14 text-lg">
+                                    Chętnie przygotujemy indywidualną ofertę i plan działania dla każdego klienta osobno, a na wasze pytania odpowiemy maksymalnie w ciągu 24 godzin.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+            <div>
+                <div className="flex flex-row flex-nowrap">
+                    <div className="w-2/12 ml-auto h-auto pt-36">
+                        <h3 className="h-auto text-2xl font-bold p-6" style={{ transform: "rotate(90deg)" }}>
+                            Napisz do nas.
+                        </h3>
+                    </div>
+                    <div className="w-10/12">
+                        <div className="max-w-6xl mr-auto">
+                            <div className="p-14 bg-gray-100 border-b-2 border-white">
+                                <h4 className="text-2xl mb-3 font-bold">
+                                    O czym chcesz napisać ?
+                                </h4>
+                                <p className="text-lg mb-10">Opisz swoją działalność, albo zadaj nam pytanie.</p>
+                                <div className="flex flex-row flex-nowrap">
+                                    <textarea rows="6" type="text" placeholder="Im dokładniej opiszesz wiadomość, tym lepiej." className="w-full rounded-lg border border-gray-200 shadow-lg py-3 px-7 text-gray-800" />
+                                </div>
+                            </div>
+
+                            <div className="p-14 bg-gray-100 border-b-2 border-white">
+                                <h4 className="text-2xl mb-3 font-bold">
+                                    Przedstaw się.
+                                </h4>
+                                <p className="text-lg mb-10">Pozwól nam Ciebie poznać.</p>
+                                <div className="flex flex-row flex-nowrap">
+                                    <div className="w-6/12 pr-5 mb-5">
+                                        <input type="text" placeholder="Imię" className="w-full rounded-lg border border-gray-200 shadow-lg py-3 px-7 text-gray-800"/>
+                                    </div>
+                                    <div className="w-6/12 mb-5">
+                                        <input type="text" placeholder="Nazwisko" className="w-full rounded-lg border border-gray-200 shadow-lg py-3 px-7 text-gray-800"/>
+                                    </div>
+                                    
+                                </div>
+                                <div className="w-full">
+                                    <input type="text" placeholder="Firma (opcjonalne)" className="w-full rounded-lg border border-gray-200 shadow-lg py-3 px-7 text-gray-800"/>
+                                </div>
+                            </div>
+
+                            <div className="p-14 bg-gray-100 border-b-2 border-white">
+                                <h4 className="text-2xl mb-3 font-bold">
+                                    Informaje kontaktowe.
+                                </h4>
+                                <p className="text-lg mb-10">W jaki sposób mozemy się z Tobą skontaktować.</p>
+                                <div className="w-full mb-5">
+                                    <input type="email" placeholder="E-mail" className="w-full rounded-lg border border-gray-200 shadow-lg py-3 px-7 text-gray-800"/>
+                                </div>
+                                <div className="w-full">
+                                    <input type="phone" placeholder="Telefon (opcjonalne)" className="w-full rounded-lg border border-gray-200 shadow-lg py-3 px-7 text-gray-800"/>
+                                </div>
+                            </div>
+                            <div className="p-14 bg-gradient-to-b from-gray-200 to-gray-white border-b-2 border-white">
+                                <button className="w-full py-3 px-8 bg-gray-900 hover:bg-gray-800 text-gray-100 hover:text-gray-200 rounded-lg">Wyślij wiadomość</button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container mx-auto px-4 md:px-14">
+                <div className="my-32">
+                    <div className="flex flex-row flex-nowrap">
+                        <div className="w-4/12 pr-10 lg:pr-20">
+                            <div className="mb-10 text-right">
+                                <p className="text-base mb-2">Nazwa firmy</p>
+                                <h6 className="text-2xl">Hukow</h6>
+                            </div>
+                        </div>
+                        <div className="w-8/12">
+                            <h1 className="text-6xl font-bold">Kilka informacji</h1>
+                            <h1 className="text-6xl font-bold">o nas.</h1>
+                        </div>
+                    </div>
+                    <div className="flex flex-row flex-nowrap mt-16">
+                        <div className="w-4/12 text-right pr-10 lg:pr-20 border-r border-gray-300">
+                            <p className="text-base mb-2">Nasza lokalizacja</p>
+                            <h6 className="text-2xl">ul. Warszawska 00</h6>
+                            <h6 className="text-2xl">15-000, Białystok</h6>
+                        </div>
+                        <div className="w-8/12 pl-10 lg:pl-20">
+                            <p className="text-base mb-2">Nip</p>
+                            <h6 className="text-2xl">987463829485</h6>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
             
                 <div className="container">
                     <div className="py4">
