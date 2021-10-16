e1ebeab0d4ef5736cc4f93fb209f943524f3a047
michals-src <54778121+michals-src@users.noreply.github.com>
2021-10-16 18:41:04 +0200

    stopka

--- src/sections/homepage/analytics.js
+++ src/sections/homepage/analytics.js
@@ -123,15 +123,15 @@ export default function Analytics() {
           </div>
           <div className="w-full md:w-6/12 md:-ml-0">
             <div className="h-full p-4 h-full">
-              <div className="flex flex-col justify-center h-full p-14 bg-green-100 rounded-md">
-                <div className="px-4 md:pl-16 text-center">
-                  <div className="my-14 text-green-800">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-current m-auto h-28 w-28" viewBox="0 0 20 20" fill="currentColor">
+              <div className="flex flex-col justify-left h-full p-14 bg-green-900 text-white rounded-md">
+                <div className="">
+                  <div className="my-14 text-green-100">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-current h-28 w-28" viewBox="0 0 20 20" fill="currentColor">
                       <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.736 6.979C9.208 6.193 9.696 6 10 6c.304 0 .792.193 1.264.979a1 1 0 001.715-1.029C12.279 4.784 11.232 4 10 4s-2.279.784-2.979 1.95c-.285.475-.507 1-.67 1.55H6a1 1 0 000 2h.013a9.358 9.358 0 000 1H6a1 1 0 100 2h.351c.163.55.385 1.075.67 1.55C7.721 15.216 8.768 16 10 16s2.279-.784 2.979-1.95a1 1 0 10-1.715-1.029c-.472.786-.96.979-1.264.979-.304 0-.792-.193-1.264-.979a4.265 4.265 0 01-.264-.521H10a1 1 0 100-2H8.017a7.36 7.36 0 010-1H10a1 1 0 100-2H8.472c.08-.185.167-.36.264-.521z" clipRule="evenodd" />
                     </svg>
                   </div>
                   <div className="my-4"><h1 className="text-2xl font-bold">Cała Europa na wyciągnięcie ręki.</h1></div>
-                  <p className="text-lg text-black">Amazon otworzył rynki w 6 europejskich krajach Niemczech, Wielkiej Brytanii, Francji, Włoszech, Hiszpanii oraz Holandii. Dzięki takiej infrastrukturze, sprzedając na platformie jesteśmy w stanie dotrzeć, aż do 28 krajów leżących w Europie.</p>
+                  <p className="text-lg">Amazon otworzył rynki w 6 europejskich krajach Niemczech, Wielkiej Brytanii, Francji, Włoszech, Hiszpanii oraz Holandii. Dzięki takiej infrastrukturze, sprzedając na platformie jesteśmy w stanie dotrzeć, aż do 28 krajów leżących w Europie.</p>
                 </div>
               </div>
             </div>
